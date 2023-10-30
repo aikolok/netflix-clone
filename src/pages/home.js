9bdcf388a1ceffb7fa97ddc97388843109c3c869
aikolok <skybloom5878@gmail.com>
2023-10-29 23:58:25 -0500

    header and feature

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Feature, OptForm } from "../components";
+import { HeaderContainer } from "../containers/header";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FooterContainer } from "../containers/footer";
 import { FaqsContainer } from "../containers/faqs";
@@ -6,6 +8,20 @@ import { FaqsContainer } from "../containers/faqs";
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>Unlimited movies, TV shows, and more</Feature.Title>
+          <Feature.SubTitle>Watch anywhere. Cancel anytime.</Feature.SubTitle>
+          <OptForm>
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </OptForm.Text>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Get Started</OptForm.Button>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
